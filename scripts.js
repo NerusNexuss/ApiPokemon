edbb3f60fad7b63115433d4653da113145d6da0b
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-11-27 08:45:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ class api {
     console.log(response.sprites)
     let Elementos = "";
     if (especifico !== undefined) {
-      // Adiciona a div ao elemento "resultadoPokemon"
+ 
       document.querySelector(`#${idPokemon}`).innerHTML = `<div>${especifico}<img src="${response.sprites.front_default}"</div>`;
 
       return response;
@@ -20,27 +20,26 @@ class api {
         return `${elemento.name}`;
       })
 
-      // Cria uma div para exibir os nomes dos Pokémon
       let pokemonDiv = document.createElement("div");
-      pokemonDiv.style.display = "flex"; // Adiciona estilo flexbox para exibir os nomes lado a lado
+     
 
       let contador = 0;
       let rowElement = document.createElement("div");
       rowElement.className = "row";
 
       for (let x = 0; x < Elementos.length; x = x + 1) {
-        // Cria um novo elemento span para exibir o nome do Pokémon
+        
         let spanElement = document.createElement("span");
         spanElement.className = "col";
-        spanElement.style.padding = "25px"; // Adiciona um pequeno espaço interno para o span
-        // Adiciona uma borda ao span
+        spanElement.style.padding = "25px";
+   
         spanElement.innerHTML = `<button class="btn btn-danger" value=${Elementos[x]} id = "pokemon${x}">${Elementos[x]}</button>`;
 
-        // Adiciona o span à div
+     
         rowElement.appendChild(spanElement);
         contador++;
 
-        // Cria uma nova linha quando atingir 5 elementos
+    
         if (contador === 5) {
           pokemonDiv.appendChild(rowElement);
           rowElement = document.createElement("div");
@@ -49,12 +48,11 @@ class api {
         }
       }
 
-      // Adiciona a última linha à div, se houver elementos restantes
+     
       if (contador > 0) {
         pokemonDiv.appendChild(rowElement);
       }
 
-      // Adiciona a div ao elemento "resultadoPokemon"
       resultadoPokemon.appendChild(pokemonDiv);
 
       return response;
